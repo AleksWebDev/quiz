0246fa0bb3477edd8eefad96f982cf71eec3a3b2
AleksWebDev <lavrovweb@gmail.com>
2024-05-19 23:22:41 +0300

    progress bar is ready

--- js/quiz.js
+++ js/quiz.js
@@ -1,17 +1,24 @@
 const cards = document.querySelectorAll('.plate');
 
-let currentIndex = 0;
+let currentIndex = 0; // Для перемещения по карточкам
+let currentCard = 0; // Для прогресс бара
 
 // show first card
 cards[currentIndex].classList.add('visible');
 
 cards[0].querySelector('[data-nav="prev"]').remove();
 
+// Запускаем прогресс бар
+upDateProgressBar();
+
 window.addEventListener('click', function(e){
     
     
     //next card
     if(e.target.closest('[data-nav="next"]')){
+
+        //Запускаем прогресс бар
+        
 
         if(currentIndex === cards.length - 1){
             return;
@@ -21,36 +28,48 @@ window.addEventListener('click', function(e){
         const answersWrapper = cards[currentIndex].querySelector('[data-answers]');
 
         if(result){
-            // hide current card
-            cards[currentIndex].classList.remove('visible');
-            currentIndex = currentIndex + 1;
-            //show next card
-            cards[currentIndex].classList.add('visible');
-            answersWrapper.classList.remove('required');
+
+            upDateProgressBar('next');
+
+            //Перемещение
+            setTimeout(function(){
+                // hide current card
+                cards[currentIndex].classList.remove('visible');
+                currentIndex = currentIndex + 1;
+                //show next card
+                cards[currentIndex].classList.add('visible');
+                answersWrapper.classList.remove('required');
+
+            }, 500);
         }else{
             answersWrapper.classList.add('required');
         }
 
     }
+
+
     //prev card
     if(e.target.closest('[data-nav="prev"]')){
         console.log('prev');
+
+        upDateProgressBar('prev');
 
         if(currentIndex === 0){
             return;
         }
 
-        cards[currentIndex].classList.remove('visible');
+        setTimeout(function(){
+            //Перемещение между карточками
+            cards[currentIndex].classList.remove('visible');
 
-        currentIndex = currentIndex - 1;
+            currentIndex = currentIndex - 1;
 
-        cards[currentIndex].classList.add('visible');
+            cards[currentIndex].classList.add('visible');
+        }, 500);
     }
 
 
 })
-
-
 
 function checkOnAnswers(card){
 
@@ -75,4 +94,30 @@ function checkOnAnswers(card){
     }
 }
 
-checkOnAnswers();
+
+function upDateProgressBar(direction = 'start'){
+
+    if(direction === 'next'){
+        currentCard = currentCard + 1;
+    }else if(direction === 'prev'){
+        currentCard = currentCard - 1;
+    }
+
+    const progressValue = document.querySelectorAll('.progress__label strong');
+    
+    const progressLineBar = document.querySelectorAll('.progress__line-bar');
+    
+    const countableCardsLength = document.querySelectorAll('[data-progress]').length;
+
+    const progress = Math.round((currentCard * 100) / countableCardsLength);
+    
+    progressValue.forEach(item => {
+        item.innerText = progress + '%';
+    })
+
+    progressLineBar.forEach(items => {
+        items.style.width = progress + '%';
+    })
+}
+
+
